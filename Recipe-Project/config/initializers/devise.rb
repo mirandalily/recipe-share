b7b1e43e7fa80e622418ab742f618100c3630b47
mirandalily <mirandaraymond@me.com>
2016-06-03 17:52:01 -0700

    omniauth working

--- Recipe-Project/config/initializers/devise.rb
+++ Recipe-Project/config/initializers/devise.rb
@@ -265,4 +265,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, "1544365689204546", "303c253360fec8d3abd81acff5193db3"
 end
--- Recipe-Project/config/routes.rb
+++ Recipe-Project/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :categories
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'recipes#index'
   resources :recipes
 end
