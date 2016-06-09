643952dcfc9e6c8b7e019c8b3a9443efcf917d4a
mirandalily <mirandaraymond@me.com>
2016-06-09 16:57:13 -0700

    clean up migrations

--- db/migrate/20160603185447_recipe_categories.rb
+++ db/migrate/20160603185447_recipe_categories.rb
@@ -1,7 +1,7 @@
 class RecipeCategories < ActiveRecord::Migration
   def change
     create_table :recipe_categories do |t|
-      t.integer :category_id, index: true, foreign_key: truee
+      t.integer :category_id, index: true, foreign_key: true
       t.integer :recipe_id, index: true, foreign_key: true
     end
   end
